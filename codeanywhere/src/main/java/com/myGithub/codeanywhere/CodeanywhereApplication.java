17ee10c20739628975b452e7de6c0e53495281b7
Xing <xgao058@ucr.edu>
2023-12-07 19:42:24 -0600

    spring initializer, web

---
+++ codeanywhere/src/main/java/com/myGithub/codeanywhere/CodeanywhereApplication.java
@@ -0,0 +1,13 @@
+package com.myGithub.codeanywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeanywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeanywhereApplication.class, args);
+	}
+
+}
